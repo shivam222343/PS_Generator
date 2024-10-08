a59f766abb2dc13033dbe4760f96e064cfa19ad8
Shivam Dombe <shivamdombe1@gmail.com>
2024-10-08 23:40:44 +0530

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-red-400 font-bold text-center text-4xl'>Hello world</div>
+    </>
+  )
+}
+
+export default App
